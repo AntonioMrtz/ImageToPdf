df2c24a32f42a4768d8a88719aa7be3d6258d610
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2022-06-19 12:19:11 +0200

    falta renombrar pngs para meter en carpeta

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
 import os
+import logging
+import shutil
 
 # You can use RIPSMC hromium to download 
 
-folder=os.fsencode("E:\RIP MSC tmo\Descargas\\")
+folder=os.fsencode("E:\RIP MSC tmo\Descargas\\")     #* Put here the folder path that contains all the folders with the pictures
 foldername=folder.decode()
 
+logging.basicConfig(level=logging.DEBUG)
 
 #? MEJORA poner interfaz grafica
 #? MEJORA seleccion de carpeta graficamentw
 
-#! PROBLEMON , QUE PASA CON CAPITULOS 7.5 , SE METEN DELANTE? parece que no -> testear
+#! The intermediate chapters like 7.5 are stored after the previous one 7 ( if this dont work in your OS , intermediate chapter would be stored
+#! before the previuos)
 
-def main():
-
-    #TODO PONER VERSION FINAL
-    '''
-    print("Welcome to ImageToPdf!\nWhat would you want to do?\n\n1.Organize images of multiple folders\n2.Convert pics to pdf")
-    entryCode=input()
-
-    if entryCode==0:
-        organizePics()
-    elif entryCode==1:
-        convertToPdf()
-    '''
-    organizePics()
 
 
 def organizePics():
 
-    #! crear carpeta final
+    # Create output folder
+
+    if not os.path.isdir(foldername+"\\"+"ImageToPdf"):
+
+        os.mkdir(foldername+"\\"+"ImageToPdf")
+
+    else: # delete contents of folder if it exists
+
+        for f in os.listdir(foldername+"\\"+"ImageToPdf"):
+            os.remove(os.path.join(foldername+"\\"+"ImageToPdf", f))
+
+    imageToPdf_folder_name=foldername+"\\"+"ImageToPdf"
+    
+
+    # iterating trough the subfolders
 
     for file in os.listdir(folder):
 
         filename=file.decode()
         absolute_path_file=foldername+filename
 
-        if os.path.isdir(absolute_path_file):
+        if os.path.isdir(absolute_path_file):   # only considering subfolders
             
-            #* entrar carpeta
-            #* recorrer y tratar , no tratar subcarpetas
-
             folder_nested=os.fsencode(absolute_path_file)
 
             for file_nested in os.listdir(folder_nested):
@@ -47,19 +49,43 @@ def organizePics():
                 file_nested_name="\\"+file_nested.decode()
                 absolute_path_file_nest=absolute_path_file+file_nested_name
 
-                #if absolute_path_file_nest in [".png" or ".jpg"]:
-                #! FALTA comprobar si png y añadir a la carpeta grande
+                if ".png" in absolute_path_file_nest or ".jpg" in absolute_path_file_nest:
+                    #! FALTA renombrar png 
+                    logging.debug(absolute_path_file_nest)
 
-                print(absolute_path_file_nest)
+                    #os.rename(old_name, new_name)  # cambio nombre para que no se sobreescriban
+                    shutil.copyfile(absolute_path_file_nest,imageToPdf_folder_name+"\\"+file_nested_name)
 
         else:
             os.remove(absolute_path_file)     
 
 def convertToPdf():
 
-    #TODO pedir carpetas , importar libreria to PDF
+    #TODO after getting folder name , generate pdf 
+    #TODO imageToPdf will be needed
+    #TODO search module generate pdf python
     pass
 
+params = {
+
+    '0' : organizePics,
+    '1' : convertToPdf
+
+}
+
+def main():
+    
+    print("Welcome to ImageToPdf!\nWhat would you want to do?\n\n0.Organize images of multiple folders\n1.Convert pics to pdf")
+    entryCode=input()
+
+    
+    function=params.get(entryCode,-1)
+
+    if(function==-1):
+        print("mal numero pa")
+        return -1
+    else:
+        function()
 
 
 if __name__ == "__main__":
